defe30aee1bfe41f31cf7938eec6a6bdd9451bf6
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-05-13 13:28:17 +1000

    Initial Commit/Setting up Frontend

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import Navbar from 'scenes/navbar';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <div>HOMEPAGE</div>
+    </Box>
+  );
+};
+
+export default HomePage;
